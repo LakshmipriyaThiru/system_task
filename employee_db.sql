1c6b90ba1390832e01fa2da00420711c0002811d
LakshmipriyaThiru <144547971+LakshmipriyaThiru@users.noreply.github.com>
2024-06-27 22:26:11 +0530

    Update employee_db.sql

--- employee_db.sql
+++ employee_db.sql
@@ -1,32 +1,3 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jun 26, 2024 at 03:46 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `employee_db`
---
-
--- --------------------------------------------------------
-
---
--- Table structure for table `employees`
---
-
 CREATE TABLE `employees` (
   `id` int(11) NOT NULL,
   `name` varchar(100) NOT NULL,
@@ -36,13 +7,9 @@ CREATE TABLE `employees` (
   `marital_status` enum('Single','Married','Divorced') NOT NULL,
   `address` varchar(255) DEFAULT NULL,
   `salary` decimal(10,2) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ;
 
--- --------------------------------------------------------
 
---
--- Table structure for table `salaries`
---
 
 CREATE TABLE `salaries` (
   `id` int(11) NOT NULL,
@@ -50,50 +17,27 @@ CREATE TABLE `salaries` (
   `salary_amount` decimal(10,2) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
---
--- Indexes for dumped tables
---
 
---
--- Indexes for table `employees`
---
+
+
 ALTER TABLE `employees`
   ADD PRIMARY KEY (`id`);
 
---
--- Indexes for table `salaries`
---
+
 ALTER TABLE `salaries`
   ADD PRIMARY KEY (`id`),
   ADD KEY `employee_id` (`employee_id`);
 
---
--- AUTO_INCREMENT for dumped tables
---
 
---
--- AUTO_INCREMENT for table `employees`
---
 ALTER TABLE `employees`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- AUTO_INCREMENT for table `salaries`
---
+
 ALTER TABLE `salaries`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
---
--- Constraints for dumped tables
---
 
---
--- Constraints for table `salaries`
---
 ALTER TABLE `salaries`
   ADD CONSTRAINT `salaries_ibfk_1` FOREIGN KEY (`employee_id`) REFERENCES `employees` (`id`);
 COMMIT;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
